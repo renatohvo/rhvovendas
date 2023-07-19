b30e65ad4826fc08a3375e4b18215f3dafe4452a
renatohvo <renatohvo@gmail.com>
2023-07-18 23:14:06 -0300

    bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>RHVOVendas</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div>
+        <h1 className="text-primary">RHVOVendas</h1>
       </div>
-      <p className="read-the-docs">
-        DevSuperior
-      </p>
-    </>
   )
 }
 
