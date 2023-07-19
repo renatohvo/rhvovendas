56ad5eee6b81141837ce34ad0607f21a9e872b9c
renatohvo <renatohvo@gmail.com>
2023-07-19 19:03:03 -0300

    model & database seed

---
+++ backend/src/main/java/com/renatohvo/rhvovendas/model/Sale.java
@@ -0,0 +1,31 @@
+package com.renatohvo.rhvovendas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_sales")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sale {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+}
---
+++ backend/src/main/java/com/renatohvo/rhvovendas/model/Seller.java
@@ -0,0 +1,28 @@
+package com.renatohvo.rhvovendas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_sellers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Seller {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales = new ArrayList<>();
+
+}
